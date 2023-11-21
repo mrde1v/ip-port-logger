ccebc7573a7c69520cd407f89fc77815166a072d
PaN-De1v <dejvklima@gmail.com>
2023-11-21 19:33:53 +0000

    feat: implemented worker

--- main.go
+++ main.go
@@ -11,14 +11,16 @@ import (
 )
 
 const (
-	listenPort  = "0.0.0.0:6969"
-	packetLimit = 3
-	logFileName = "ips.txt"
+	listenAddress = "0.0.0.0:6969"
+	packetLimit   = 1
+	logFileName   = "ips.txt"
+	numWorkers    = 10
 )
 
 var (
 	packetCounts map[string]int
 	packetMutex  sync.Mutex
+	logMutex     sync.Mutex
 )
 
 func main() {
@@ -26,25 +28,27 @@ func main() {
 
 	go startServer()
 
+	startWorkerPool()
+
 	go monitorPackets()
 
 	select {}
 }
 
 func startServer() {
-	listener, err := net.Listen("tcp", listenPort)
+	listener, err := net.Listen("tcp", listenAddress)
 	if err != nil {
-		fmt.Println("Error starting server: ", err)
+		fmt.Println("Error starting server:", err)
 		os.Exit(1)
 	}
 	defer listener.Close()
 
-	fmt.Printf("Server listening on %s\n", listenPort)
+	fmt.Printf("Server listening on %s\n", listenAddress)
 
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println("Error accepting connection: ", err)
+			fmt.Println("Error accepting connection:", err)
 			continue
 		}
 
@@ -58,60 +62,80 @@ func handleConnection(conn net.Conn) {
 	remoteAddr := conn.RemoteAddr().String()
 	fmt.Printf("Connection from %s\n", remoteAddr)
 
+	reader := bufio.NewReader(conn)
+
 	for {
-		data, err := bufio.NewReader(conn).ReadString('\n')
+		_, err := reader.ReadString('\n')
 		if err != nil {
 			if err.Error() == "EOF" {
-				processData(remoteAddr, data)
+				processData(remoteAddr)
+				fmt.Printf("Connection from %s closed by the client\n", remoteAddr)
 				return
 			}
-			fmt.Println("Error reading from connection: ", err)
+			fmt.Println("Error reading from connection:", err)
 			return
 		}
 
-		processData(remoteAddr, data)
+		go processData(remoteAddr)
 	}
 }
 
-func processData(remoteAddr, data string) {
+func startWorkerPool() {
+	for i := 0; i < numWorkers; i++ {
+		go worker()
+	}
+}
+
+func worker() {
+	for {
+		select {
+		case job := <-jobQueue:
+			processData(job.remoteAddr)
+		}
+	}
+}
+
+type job struct {
+	remoteAddr string
+}
+
+var jobQueue = make(chan job, 100)
+
+func processData(remoteAddr string) {
 	packetMutex.Lock()
 	packetCounts[remoteAddr]++
 	packetMutex.Unlock()
 
-	fmt.Printf("Received data from %s: %s\n", remoteAddr, data)
-}
+	fmt.Println(packetCounts[remoteAddr])
 
-func monitorPackets() {
-	for {
-		time.Sleep(time.Second)
-
-		packetMutex.Lock()
-		for ip, count := range packetCounts {
-			if count > packetLimit {
-				logIP(ip)
-			}
-			packetCounts[ip] = 0
-		}
-		packetMutex.Unlock()
+	if packetCounts[remoteAddr] > packetLimit {
+		logIP(remoteAddr)
+		packetCounts[remoteAddr] = 0
 	}
 }
 
 func logIP(ip string) {
-	if ipExistsInFile(ip) {
+	ipport := strings.Split(ip, ":")
+	if ipExistsInFile(ipport[0]) {
 		return
 	}
 
+	fmt.Println("logging")
+
+	logMutex.Lock()
+	defer logMutex.Unlock()
+
 	file, err := os.OpenFile(logFileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
-		fmt.Println("Error opening log file: ", err)
+		fmt.Println("Error opening log file:", err)
 		return
 	}
 	defer file.Close()
 
-	if _, err := file.WriteString(ip + "\n"); err != nil {
-		fmt.Println("Error writing to log file: ", err)
+	if _, err := file.WriteString(ipport[0] + "\n"); err != nil {
+		fmt.Println("Error writing to log file:", err)
 	}
-	fmt.Printf("IP %s exceeded packet limit\n", ip)
+	fmt.Printf("IP %s exceeded packet limit and is logged in %s\n", ip, logFileName)
 }
 
 func ipExistsInFile(ip string) bool {
@@ -129,4 +153,19 @@ func ipExistsInFile(ip string) bool {
 	}
 
 	return false
+}
+
+func monitorPackets() {
+	for {
+		time.Sleep(time.Second)
+
+		packetMutex.Lock()
+		for ip, count := range packetCounts {
+			if count > packetLimit {
+				logIP(ip)
+			}
+			packetCounts[ip] = 0
+		}
+		packetMutex.Unlock()
+	}
 }
